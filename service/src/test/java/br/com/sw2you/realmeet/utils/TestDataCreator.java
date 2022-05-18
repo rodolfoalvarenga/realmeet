964e1c0603082d708d6f6c35d900163395e789e8
RODOLFO LUIZ ALVARENGA <t-rodolfo.alvarenga@bradesco.com.br>
2022-05-18 15:18:26 -0300

    Initial features

---
+++ service/src/test/java/br/com/sw2you/realmeet/utils/TestDataCreator.java
@@ -0,0 +1,14 @@
+package br.com.sw2you.realmeet.utils;
+
+import br.com.sw2you.realmeet.domain.entity.Room;
+
+import static br.com.sw2you.realmeet.utils.TestConstants.*;
+
+public final class TestDataCreator {
+
+    private TestDataCreator() {}
+
+    public static Room.Builder newRoomBuilder() {
+        return Room.newBuilder().name(DEFAULT_ROOM_NAME).seats(DEFAULT_ROOM_SEATS);
+    }
+}
